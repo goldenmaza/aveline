2014c7eabaa9af68bd5363b423ead5a73c196206
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-04-27 11:30:00 +0200

    [Aveline] BrowserRouter changes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,120 +1,40 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-import Header from './common/Header';
-import Nav from './common/Nav';
 import Heading from './common/Heading';
-import Handler from './common/Handler';
-import Menu from './common/Menu';
+import Header from './common/Header';
 import Section from './common/Section';
 import Footer from './common/Footer';
-import Container from './common/Container';
-import Contact from './common/Contact';
-import Social from './common/Social';
-import Sitemap from './common/Sitemap';
-import Brand from './common/Brand';
-import Copyright from './common/Copyright';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            availableOptions: null
+            level: 'h1',
+            label: 'Aveline',
+            tag: null
         };
-        this.apiCall();
     }
 
-    apiCall() {
-        const requestBody = {
-            query: `
-                query {
-                    page {
-                        id
-                        label
-                        layout
-                        hidden
-                        title
-                        aria
-                    }
-                    content {
-                        id
-                        page
-                        header
-                        text
-                        hidden
-                    }
-                    multimedia {
-                        id
-                        page
-                        src
-                        ordering
-                        box
-                        hidden
-                        title
-                        aria
-                    }
-                }
-            `
-        };
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify(requestBody)
-        };
-        //console.log(options);
-        fetch('http://localhost:6969/api', options).then(promise => {
-            return promise.json();
-//            if (res.ok) {
-//                return res.json();
-//            } else {
-//                console.log(res);
-//                throw new Error("Unable to fetch data from GraphQL API...");
-//            }
-        }).then(result => {
-            console.log(result.data);
+    componentDidUpdate(prevProps) {
+        if (this.props.location.pathname !== prevProps.location.pathname) {
             this.setState({
-                availableOptions: result.data.page
+                tag: this.props.location.pathname.split("/").pop()
             });
-            //console.log(json);
-        });
+        }
     }
 
     render() {
+        const { level, label } = this.state;
         return (
-            <Fragment>
-                <Header>
-                    <Nav>
-                        <Heading>
-                        </Heading>
-                        <Handler>
-                        </Handler>
-                        <Menu>
-                        </Menu>
-                    </Nav>
-                </Header>
-                <Section>
-                </Section>
-                <Footer>
-                    <Container>
-                        <Contact>
-                        </Contact>
-                        <Social>
-                        </Social>
-                    </Container>
-                    <Container>
-                        <Sitemap>
-                        </Sitemap>
-                        <Brand>
-                        </Brand>
-                    </Container>
-                    <Copyright>
-                    </Copyright>
-                </Footer>
-            </Fragment>
+            <>
+                <Heading hidden={true} level={level} label={label} />
+                <Header />
+                <Section tag={this.state.tag} />
+                <Footer />
+            </>
         );
     }
 }
 
-export default App;
+export default withRouter(props => <App {...props}/>);
