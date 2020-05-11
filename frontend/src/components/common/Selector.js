2616095730f34fabf2c16517125645867752c370
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-05-11 20:11:04 +0200

    [Aveline] Refactoring of nested components

--- frontend/src/components/common/Selector.js
+++ frontend/src/components/common/Selector.js
@@ -1,33 +1,53 @@
 import React, { Component } from 'react';
 
+import Content from './Content';
 import Header from './Header';
+import Heading from './Heading';
 import Home from './Home';
 import Form from '../form/Form';
-import Page from './Page';
 import Footer from './Footer';
 
 class Selector extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            loading: true,
+            level: 'h2',
+            label: 'Page content'
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            loading: false
+        });
     }
 
     render() {
-        const component = this.props.component.trim();
-        return (
-            <>
-                <Header />
-                {component === 'Home' &&
-                    <Home tag={this.props.tag} />
-                }
-                {component === 'Form' &&
-                    <Form tag={this.props.tag} />
-                }
-                {component === 'Page' &&
-                    <Page tag={this.props.tag} />
-                }
-                <Footer />
-            </>
-        );
+        if (this.state.loading) {
+            return (<div></div>); // Refactor to display loading animation...minimalContact
+        } else {
+            const { level, label } = this.state;
+            const component = this.props.component.trim();
+            return (
+                <>
+                    <Header />
+                    <main>
+                        <Heading hidden={true} level={level} label={label} />
+                        {component === 'Home' &&
+                            <Home tag={this.props.tag} />
+                        }
+                        {component === 'Form' &&
+                            <Form tag={this.props.tag} />
+                        }
+                        {component === 'Content' &&
+                            <Content tag={this.props.tag} />
+                        }
+                    </main>
+                    <Footer />
+                </>
+            );
+        }
     }
 }
 
