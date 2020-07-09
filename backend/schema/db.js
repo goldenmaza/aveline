a332b7b5b690381bddcb165e1fed9fde1879e248
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-07-09 22:25:04 +0200

    [Aveline] Refactoring of paths to location of definitions

--- backend/schema/db.js
+++ backend/schema/db.js
@@ -3,13 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Sequelize definitions...
-import { seqPage } from './sequelize/page';
-import { seqContent } from './sequelize/content';
-import { seqMultimedia } from './sequelize/multimedia';
-import { seqOffice } from './sequelize/office';
-import { seqContact } from './sequelize/contact';
-import { seqSocial } from './sequelize/social';
-import { seqNavigation } from './sequelize/navigation';
+import { seqPage } from './definitions/page';
+import { seqContent } from './definitions/content';
+import { seqMultimedia } from './definitions/multimedia';
+import { seqOffice } from './definitions/office';
+import { seqContact } from './definitions/contact';
+import { seqSocial } from './definitions/social';
+import { seqNavigation } from './definitions/navigation';
 
 //TODO: Refector to use for dev, test and prod...
 //const env = process.env.NODE_ENV || 'development';
@@ -17,19 +17,23 @@ import { seqNavigation } from './sequelize/navigation';
 //config.database (same as process.env.SEQUELIZE_DATABASE) etc
 
 // Create a new Sequelize instance to setup the connection with a database (localhost)...
-const db = new Sequelize(process.env.SEQUELIZE_DATABASE, process.env.SEQUELIZE_USERNAME, process.env.SEQUELIZE_PASSWORD, {
-    dialect: process.env.SEQUELIZE_DIALECT,
-    host: process.env.SEQUELIZE_HOST,
-    port: process.env.SEQUELIZE_PORT,
-    pool: {
-        max: 7,
-        min: 0,
-        idle: 6000
-    },
-    define: {
-        timestamps: false
+const db = new Sequelize(
+    process.env.SEQUELIZE_DATABASE,
+    process.env.SEQUELIZE_USERNAME,
+    process.env.SEQUELIZE_PASSWORD, {
+        dialect: process.env.SEQUELIZE_DIALECT,
+        host: process.env.SEQUELIZE_HOST,
+        port: process.env.SEQUELIZE_PORT,
+        pool: {
+            min: process.env.SEQUELIZE_POOL_MIN,
+            max: process.env.SEQUELIZE_POOL_MAX,
+            idle: process.env.SEQUELIZE_POOL_IDLE
+        },
+        define: {
+            timestamps: false
+        }
     }
-});
+);
 
 // Validate the connection...
 db.authenticate().then((val) => {
@@ -38,7 +42,7 @@ db.authenticate().then((val) => {
     console.error("Unable to connect to the database: ", err);
 });
 
-// Declarations of definitions for Sequelize types...
+// Declarations of definitions for Sequelize models...
 const Page = db.define('page', seqPage);
 const Content = db.define('content', seqContent);
 const Multimedia = db.define('multimedia', seqMultimedia);
