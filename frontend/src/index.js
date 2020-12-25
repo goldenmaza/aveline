f7b7cacf46556d5f62ae9440cb7414e56f959ee9
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-12-25 03:21:04 +0100

    [Aveline] Development/Production setup for the frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
-
 import App from './components/App';
+import path from 'path';
+import dotenv from 'dotenv';
+
+const environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+dotenv.config({ path: path.join(__dirname, `../.env.${environment}`) });
 
 window.addEventListener('load', () => {
     ReactDOM.render(
