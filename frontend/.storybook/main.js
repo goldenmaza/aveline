1cbe88d8bf154ea095513dc8237ba675ed89656d
Mats Richard Hellstrand <goldenmaza@outlook.com>
2021-01-24 14:31:09 +0100

    [Storybook] Installation of Storybook dependency

---
+++ frontend/.storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/test/stories/components/**/*.stories.mdx",
+    "../src/test/stories/components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
