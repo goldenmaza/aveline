f4f110edb13dc8c60a3fd81e75e633988c698d2f
Mats Richard Hellstrand <goldenmaza@outlook.com>
2021-01-26 13:13:48 +0100

    [Storybook] Added a way of sorting stories

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,5 +1,20 @@
 import '../src/scss/index.scss';
 
 export const parameters = {
-    actions: { argTypesRegex: "^on[A-Z].*" }
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    options: {
+        storySort: {
+            order: [
+//                "Header", ["HeaderComponent", "HandlerComponent", "MenuComponent"],
+//                "Form", ["FormComponent", "HelpSectionComponent"],
+//                "Footer", ["FooterComponent", "SitemapComponent", "CopyrightComponent"]
+                "Common - 0", "Common - 1", "Common - 2", "Common - 3", "Common - 4",
+                "Header - 0", "Header - 1", "Header - 2",
+                "Home - 0", "Home - 1", "Home - 2",
+                "Contact - 0",
+                "Form - 0", "Form - 1",
+                "Footer - 0", "Footer - 1", "Footer - 2"
+            ]
+        }
+    }
 }
