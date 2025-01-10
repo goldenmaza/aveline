a3e0d187c872b4e6790c08c0f7302314044f4f2b
Mats Richard Hellstrand <goldenmaza@outlook.com>
2025-01-10 02:04:52 +0100

    [Copyright] Functional component implementation

--- frontend/src/components/footer/Copyright.js
+++ frontend/src/components/footer/Copyright.js
@@ -1,44 +1,21 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useCopyrightComponentState } from '../../hooks/footer';
 
 import Heading from '../common/Heading';
 
-class Copyright extends Component {
-    render() {
-        const { created, level, label, name, link, by } = this.props;
-        const year = new Date().getFullYear();
-        const copy = year > created ? created + ' - ' + year : year;
-        return (// TODO: Refactor '/' && 'noopener noreferrer' to env???
-            <>
-                <Heading hidden={true} level={level} label={label} />
-                <div className='footer_copyright'>
-                    <strong>&copy; {copy} <a href='/'>{name}</a>. All rights reserved.</strong>
-                </div>
-                <div className='footer_by'>
-                    <span>by <a href={link} target='_blank' rel='noopener noreferrer'>{by}</a></span>
-                </div>
-            </>
-        );
-    }
+export default function Copyright() {
+    const { created, level, label, name, link, by } = useCopyrightComponentState();
+
+    const year = new Date().getFullYear();
+    const copy = year > created ? created + ' - ' + year : year;
+    return (// TODO: Refactor '/' && 'noopener noreferrer' to env...
+        <>
+            <Heading hidden={true} level={level} label={label} />
+            <div className='footer_copyright'>
+                <strong>&copy; {copy} <a href='/'>{name}</a>. All rights reserved.</strong>
+            </div>
+            <div className='footer_by'>
+                <span>by <a href={link} target='_blank' rel='noopener noreferrer'>{by}</a></span>
+            </div>
+        </>
+    );
 }
-
-const mapStateToProps = state => ({
-    created: state.copyrightComponent.created,
-    level: state.copyrightComponent.level,
-    label: state.copyrightComponent.label,
-    name: state.copyrightComponent.name,
-    link: state.copyrightComponent.link,
-    by: state.copyrightComponent.by
-});
-
-const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators({
-    }, dispatch)
-});
-
-export default withRouter(connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(Copyright));
