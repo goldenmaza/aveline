f3ad8df1e40e3320fd4384810d14a127932919cc
Mats Richard Hellstrand <goldenmaza@outlook.com>
2025-01-10 01:51:55 +0100

    [Prevention] Functional component implementation

--- frontend/src/components/common/Prevention.js
+++ frontend/src/components/common/Prevention.js
@@ -1,32 +1,13 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { usePreventionComponentState } from '../../hooks/common';
 
 import Heading from './Heading';
 
-class Prevention extends Component {
-    render() {
-        const { level, label } = this.props;
-        return (
-            <section>
-                <Heading hidden={false} level={level} label={label} />
-            </section>
-        );
-    }
+export default function Prevention(props) {
+    const { level, label } = usePreventionComponentState();
+
+    return (
+        <section>
+            <Heading hidden={false} level={level} label={label} />
+        </section>
+    );
 }
-
-const mapStateToProps = state => ({
-    level: state.preventionComponent.level,
-    label: state.preventionComponent.label
-});
-
-const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators({
-    }, dispatch)
-});
-
-export default withRouter(connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(Prevention));
