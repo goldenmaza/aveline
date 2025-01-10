f3ed46aab506ef5492ea2fa60601f6be92f3a14a
Mats Richard Hellstrand <goldenmaza@outlook.com>
2025-01-10 02:03:21 +0100

    [Menu] Functional component implementation

--- frontend/src/components/header/Menu.js
+++ frontend/src/components/header/Menu.js
@@ -1,70 +1,33 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { NavLink, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
-import {
-    getHeaderMenu,
-    toggleHandlerMenu
-} from '../../redux/actions/header';
+import { useMenuComponentState } from '../../hooks/header';
 
-class Menu extends Component {
-    constructor(props) {
-        super(props);
+import { toggleHandlerMenu } from '../../redux/actions/header';
 
-        this.toggleBar = this.toggleBar.bind(this);
-    }
+export default function Menu () {
+    const { loading, toggled, pages } = useMenuComponentState();
 
-    componentDidMount() {
-        this.props.actions.getHeaderMenu();
-    }
+    if (loading) {
+        return (<div></div>); // Refactor to display loading animation...
+    } else {
+        const handlerNavigation = toggled ? 'handler_navigation active' : 'handler_navigation';
 
-    toggleBar() {
-        this.props.actions.toggleHandlerMenu();
-    }
-
-    render() {
-        const { loading, toggled, pages } = this.props;
-        if (loading) {
-            return (<div></div>); // Refactor to display loading animation...
-        } else {
-            const handlerNavigation = toggled ? 'handler_navigation active' : 'handler_navigation';
-            const items = [];
-
-            pages.forEach(p => {
-                const to = '/p/' + p.label.toLowerCase().replace('å', 'a').replace('ä', 'a').replace('ö', 'o');
-                items.push(
-                    <li key={p.id}>
-                        <NavLink to={to} activeClassName={'active'} onClick={this.toggleBar} title={p.title}>
-                            {p.label}
-                        </NavLink>
-                    </li>
-                );
-            });
-
-            return (
-                <ul className={handlerNavigation}>
-                    {items}
-                </ul>
+        const items = [];
+        pages.forEach(p => {
+            const to = '/page/' + p.label.toLowerCase().replace('å', 'a').replace('ä', 'a').replace('ö', 'o');
+            items.push(
+                <li key={p.id}>
+                    <NavLink to={to} className={'active'} onClick={toggleHandlerMenu} title={p.title}>
+                        {p.label}
+                    </NavLink>
+                </li>
             );
-        }
+        });
+
+        return (
+            <ul className={handlerNavigation}>
+                {items}
+            </ul>
+        );
     }
 }
-
-const mapStateToProps = state => ({
-    loading: state.menuComponent.loading,
-    toggled: state.menuComponent.toggled,
-    pages: state.menuComponent.pages
-});
-
-const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators({
-        getHeaderMenu,
-        toggleHandlerMenu
-    }, dispatch)
-});
-
-export default withRouter(connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(Menu));
