c660bfb2f00a65a570411ca6ae91b5134d8438a0
Mats Richard Hellstrand <goldenmaza@outlook.com>
2025-01-10 02:04:02 +0100

    [Navigation] Functional component implementation

--- frontend/src/components/contact/Navigation.js
+++ frontend/src/components/contact/Navigation.js
@@ -1,31 +1,8 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+export default function Navigation(props) {
+    const { address } = props;
 
-class Navigation extends Component {
-    render() {
-        const { address } = this.props;
-        if (!address) {
-            return (<div></div>); // Refactor to display loading animation...
-        } else {
-            const src = 'https://maps.google.com/maps?q=' + address + '&t=k&z=13&ie=UTF8&iwloc=&output=embed';
-            return (
-                <iframe src={src} tabIndex='-1' title='Navigational map to find our address...'></iframe>
-            );
-        }
-    }
+    const src = 'https://maps.google.com/maps?q=' + address + '&t=k&z=13&ie=UTF8&iwloc=&output=embed';
+    return (
+        <iframe src={src} tabIndex='-1' title='Navigational map to find our address...'></iframe>
+    );
 }
-
-const mapStateToProps = state => ({
-});
-
-const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators({
-    }, dispatch)
-});
-
-export default withRouter(connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(Navigation));
