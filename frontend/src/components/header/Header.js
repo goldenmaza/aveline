2ac7af2763c9420132fd48f664ef0a6a1b9f553c
Mats Richard Hellstrand <goldenmaza@outlook.com>
2025-01-10 01:54:08 +0100

    [Header] Functional component implementation

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,36 +1,17 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useHeaderComponentState } from '../../hooks/header';
 
 import Heading from '../common/Heading';
 import Handler from './Handler';
 
-class Header extends Component {
-    render() {
-        const { level, label } = this.props;
-        return (
-            <header>
-                <nav aria-label='main navigation'>
-                    <Heading hidden={true} level={level} label={label} />
-                    <Handler />
-                </nav>
-            </header>
-        );
-    }
+export default function Header() {
+    const { level, label } = useHeaderComponentState();
+
+    return (
+        <header>
+            <nav aria-label='main navigation'>
+                <Heading hidden={true} level={level} label={label} />
+                <Handler />
+            </nav>
+        </header>
+    );
 }
-
-const mapStateToProps = state => ({
-    level: state.headerComponent.level,
-    label: state.headerComponent.label
-});
-
-const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators({
-    }, dispatch)
-});
-
-export default withRouter(connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(Header));
