b5a1302c16d147c8339a06c2a571c88654abf115
Mats Richard Hellstrand <goldenmaza@outlook.com>
2025-01-10 02:02:20 +0100

    [Collage] Functional component implementation

--- frontend/src/components/common/Collage.js
+++ frontend/src/components/common/Collage.js
@@ -1,47 +1,23 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+export default function Collage(props) {
+    const { collection } = props;
 
-class Collage extends Component {
-    render() {
-        const { collection } = this.props;
-        if (!collection) {
-            return (<div></div>); // Refactor to display loading animation...
-        } else {
-            const collage = [];
-
-            collection.forEach(c => {
-                collage.push(
-                    <a key={c.id} href={c.src} target='_blank' rel='noopener noreferrer'>
-                        <img src={c.src} alt={c.alt} title={c.title} />
-                        <div>
-                            <span>
-                                View source...
-                            </span>
-                        </div>
-                    </a>
-                );
-            });
-
-            return (
-                <div className='collageContainer'>
-                    { collage }
+    const collage = [];
+    collection.forEach(c => {
+        collage.push(
+            <a key={c.id} href={c.src} target='_blank' rel='noopener noreferrer'>
+                <img src={c.src} alt={c.alt} title={c.title} loading="lazy" />
+                <div>
+                    <span>
+                        View source...
+                    </span>
                 </div>
-            );
-        }
-    }
+            </a>
+        );
+    });
+
+    return (
+        <div className='collageContainer'>
+            { collage }
+        </div>
+    );
 }
-
-const mapStateToProps = state => ({
-});
-
-const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators({
-    }, dispatch)
-});
-
-export default withRouter(connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(Collage));
